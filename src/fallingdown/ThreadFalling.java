db5cb2066509f6150dad68241e3c38f65b620dac
antno <antno@LAPTOP-974RUBCT>
2019-11-27 17:07:25 +0100

    Creazione del jFrame

---
+++ src/fallingdown/ThreadFalling.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fallingdown;
+import java.awt.EventQueue;
+import javax.swing.*;
+/**
+ *
+ * @author antno
+ */
+public class ThreadFalling extends JFrame{
+    public ThreadFalling(){
+        initUi();
+    }
+     private void initUi() {
+        
+        add(new Board());
+
+        setResizable(false);
+        pack();
+        
+        setTitle("UnisaInvaders");    
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+    }
+     public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new ThreadFalling();
+            ex.setVisible(true);
+        });
+    }
+}
