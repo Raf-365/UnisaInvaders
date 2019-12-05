b634968f877e419cc0f0587a16c441b6b15f44f5
Raffaele <Raffaele@172.19.162.77>
2019-12-05 16:32:45 +0100

    Aggiunta dei missili

---
+++ src/fallingdown/Missile.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fallingdown;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author stefa
+ */
+public class Missile {
+    
+    private int dx;
+    private int dy;
+    private int x ;
+    private int y ;
+    private int w;
+    private int h;
+    private Image image;
+    private boolean visible;
+    
+    private static int DISTANCE_FROM_PEPPER = 5, MISSILE_SPEED=2;
+    
+    public Missile(int x, int y){
+        loadImage();
+        this.x=x;
+        this.y=y-h-DISTANCE_FROM_PEPPER;
+    }
+    
+    private void loadImage() {
+
+        ImageIcon ii = new ImageIcon("src/resources/missile2.png");
+        image = ii.getImage(); 
+        
+        visible=true;
+            
+        w = image.getWidth(null);
+        h = image.getHeight(null);       
+    }
+    
+    public void move() {
+        y -= MISSILE_SPEED;
+    }
+
+    public int getX() {return x;}
+
+    public int getY() {return y;}    
+
+    public int getWidth() {return w;}    
+
+    public int getHeight() {return h;}
+
+    public Image getImage() {return image;}
+    
+    public Rectangle getBounds() {return new Rectangle(x, y, w, h);}
+    
+    public boolean isVisible() {return visible;}
+
+    public void setVisible(Boolean visible) {this.visible = visible;}
+}
+
--- src/fallingdown/Pepper.java
+++ src/fallingdown/Pepper.java
@@ -7,6 +7,7 @@ package fallingdown;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import java.util.Observable;
 /**
@@ -23,14 +24,36 @@ public class Pepper extends Observable implements CollidableObjects {
     private int h;
     private Image image;
     private int st;
+    private Integer health;
+    private ArrayList<Missile> missilesArray;
+    private boolean  stopFiring;
 
     public Pepper() {
         loadImage();
+        missilesArray = new ArrayList<>();
     }
     public int getSt() {
         return st;
     }
 
+    public ArrayList<Missile> getMissiles(){return missilesArray;}
+    
+        private void fire(){
+        missilesArray.add(new Missile(x, y));
+    }
+    
+    public void deleteMissile(Missile m){
+        missilesArray.remove(m);
+    }
+    
+    
+    public void changeImage(int num){
+        ImageIcon ii = new ImageIcon("src/resources/Pepper"+num+".png");
+        image = ii.getImage().getScaledInstance(200, 300, Image.SCALE_SMOOTH);
+        image = ii.getImage();
+    }
+    
+        
     public void setSt(int st) {
         this.st = st;
          setChanged();
@@ -42,7 +65,7 @@ public class Pepper extends Observable implements CollidableObjects {
         ImageIcon ii = new ImageIcon("src/Resources/Pepper2.png");
         
         image = ii.getImage().getScaledInstance(200, 300, Image.SCALE_SMOOTH); 
-        
+        stopFiring=false;
         w = image.getWidth(null);
         h = image.getHeight(null);
     }
@@ -88,6 +111,12 @@ public class Pepper extends Observable implements CollidableObjects {
             dx = 7;
      
         }
+        
+        if (key == KeyEvent.VK_SPACE && !stopFiring){ 
+            changeImage(10);
+            fire(); 
+            stopFiring=true;
+        }
 
     }
 
@@ -102,13 +131,18 @@ public class Pepper extends Observable implements CollidableObjects {
         if (key == KeyEvent.VK_RIGHT) {
             dx = 0;
         }
+        
+        if (key == KeyEvent.VK_SPACE){ 
+            changeImage(20);   
+            stopFiring=false;
+        }
 
     }
     
     @Override
         public Rectangle getBounds() {
             
-        return new Rectangle(x+70, y+70, 150, 300);
+        return new Rectangle(x+40, y+30, 150, 300);
         
     }
 }
